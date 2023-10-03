206daa2a5d280c323e964c708f246ac246a171d6
Probe1st <svintsitskiy.leha@mail.ru>
2023-10-03 13:26:27 +0500

    Revert "добавление логики в хедер"
    This reverts commit bb9af67ca3c6f241f7acf932db60a36862186019.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -24,101 +24,80 @@ export default function Profile() {
 
   SetSrcImage("/profile/smart-human.png", 'smart-human');
 
-  async function setDataUser(app, auth) {
-    const snap = await getDoc(
-      doc(getFirestore(app), "/users/", auth.currentUser.email)
-    );
-    const data = snap.data();
-
-    const balance = document.querySelector("[data-balance]");
-    const { currency, number } = data.balance;
-    balance.innerHTML = number + currency;
-
-    if (data.purshe) {
-      let robot = await getDoc(doc(getFirestore(app), data.purshe));
-
-      const robotImg = robot.data().linkImage;
-
-      const img = document.querySelector("[data-bg-image='robot']");
-      img.setAttribute("src", robotImg);
-
-      const name = document.querySelector("[data-robot]");
-      name.innerHTML = robot.id;
-    }
-  }
-
   return (
     <div className="card-profile">
       {/* Ваш торговый советиник */}
-      <YourRobot />
+      <div>
+        <img data-bg-image="robot" alt="" />
+
+        <h2>Ваш торговый советник</h2>
+
+        <div className="status-bar">
+          <p data-robot>имя советника</p>
+        </div>
+      </div>
 
       {/* Баланс */}
-      <Balance />
+      <div>
+        <img data-src-image="bank" alt="" />
+
+        <h2>На вашем счету</h2>
+
+        <div className="status-bar">
+          <p data-balance></p>
+        </div>
+      </div>
 
       {/* реферал */}
-      <Referral />
+      <form>
+        <img data-src-image="smart-human" alt="" />
+
+        <div className="status-bar">
+          <p>Пригласи 3 реферала и получи <br />торгового советника бесплатно</p>
+        </div>
+
+        <input className="status-bar outline-none" type="text" name="token" id="token" />
+      </form>
 
       {/* пополнить */}
-      <Replenish />
+      <div>
+        <h2 className="justify-self-start text-2xl">Пополнить баланс</h2>
+
+        <div className="flex flex-col justify-between space-y-2 w-full text-black">
+          <Link className={"w-full bg-purple-300 px-5 py-1 rounded-full"} >Visa / MasterCard</Link>
+          <div className="flex flex-row justify-between space-x-2">
+            <Link className={"bg-white px-4 py-1 w-full rounded-3xl"} >Btc</Link>
+            <Link className={"bg-white px-4 py-1 w-full rounded-3xl"} >Usdt</Link>
+          </div>
+        </div>
+
+        <Link className={"bg-blue-500 px-20 py-2 w-fit rounded-3xl text-2xl"} >Пополнить</Link>
+      </div>
 
       <button onClick={() => { signOut(auth); navigate('/login') }}>выйти</button>
     </div>
   );
 }
 
-function YourRobot() {
-  return (
-    <div>
-      <img data-bg-image="robot" alt="" />
+async function setDataUser(app, auth) {
+  const snap = await getDoc(
+    doc(getFirestore(app), "/users/", auth.currentUser.email)
+  );
+  const data = snap.data();
 
-      <h2>Ваш торговый советник</h2>
+  const balance = document.querySelector("[data-balance]");
+  const { currency, number } = data.balance;
+  balance.innerHTML = number + currency;
 
-      <div className="status-bar">
-        <p data-robot>не приобретено</p>
-      </div>
-    </div>
-  )
-}
+  if (data.purshe) {
+    let robot = await getDoc(doc(getFirestore(app), data.purshe));
 
-function Balance() {
-  return (
-    <div>
-      <img data-src-image="bank" alt="" />
+    const robotImg = robot.data().linkImage;
 
-      <h2>На вашем счету</h2>
+    const img = document.querySelector("[data-bg-image='robot']");
+    img.setAttribute("src", robotImg);
 
-      <div className="status-bar">
-        <p data-balance></p>
-      </div>
-    </div>
-  )
-}
-
-function Referral() {
-  return (
-    <form>
-      <img data-src-image="smart-human" alt="" />
-
-      <div className="status-bar">
-        <p>Пригласи 3 реферала и получи <br />торгового советника бесплатно</p>
-      </div>
-
-      <input className="status-bar outline-none" type="text" name="token" id="token" />
-    </form>
-  )
-}
-
-function Replenish() {
-  return (
-    <div>
-      <h2 className="justify-self-start text-2xl font-semibold">Пополнить баланс</h2>
-
-      <p className="w-10/12 text-justify text-ellipsis ">
-        Для получения информации об удобных вариантах оплаты обратитесь к вашему персональному менеджеру.
-        Он предоставит вам индивидуальные реквизиты, и закрепит робота.
-      </p>
-
-      <Link to={"https://example.com"} target="_blank" className={"bg-blue-500 px-20 py-2 w-fit rounded-3xl text-2xl"}>Связаться</Link>
-    </div>
-  )
+    const name = document.querySelector("[data-robot]");
+    name.innerHTML = robot.id;
+  }
 }
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -17,7 +17,7 @@ export default function Register() {
   // handling form 
   const handleSubmit = async (e, auth, db) => {
     e.preventDefault();
-    const form = document.querySelector('form');
+    let form = e.target;
 
     let data = new Array(...form.querySelectorAll("input")).map((e) => e.value);
     data = new RegisterFormData(...data, "/robots/Default");
@@ -25,9 +25,6 @@ export default function Register() {
     const { email, pass, repPass } = dataForCreateAccount;
 
     let notice = form.querySelector("#notice");
-    
-    // const formData = new FormData(form);
-    // const [email, password] = formData.getAll("credential");
 
     if (!validatePass(pass, repPass)) {
       notice.removeAttribute("hidden");
@@ -72,14 +69,14 @@ export default function Register() {
 
         <div className="flex flex-col md:flex-row items-center space-y-3 space-x-0 md:space-y-0 md:space-x-5">
           <div className="flex flex-col items-center space-y-3">
-            <InputForm name="cardUser" type="text" id="surname" placeholder="Фамилия" />
-            <InputForm name="cardUser" type="text" id="name" placeholder="Имя" />
-            <InputForm name="cardUser" className="w-full" type="tg" id="tg" placeholder="Телеграм" />
+            <InputForm type="text" id="surname" placeholder="Фамилия" />
+            <InputForm type="text" id="name" placeholder="Имя" />
+            <InputForm className="w-full" type="tg" id="tg" placeholder="Телеграм" />
           </div>
 
           <div className="flex flex-col items-center space-y-3">
-            <InputForm name="credential" type="email" id="email" placeholder="Почта" />
-            <InputForm name="credential" type="password" id="password" placeholder="Пароль" />
+            <InputForm type="email" id="email" placeholder="Почта" />
+            <InputForm type="password" id="password" placeholder="Пароль" />
             <InputForm
               type="Password"
               id="RepeatPassword"
